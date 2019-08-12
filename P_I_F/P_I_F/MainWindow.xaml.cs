a81b9ca2d005672533476c1243579a2b9322c9e1
H5Y0UNG <wjavmyosy01@gmail.com>
2019-08-12 19:56:16 +0900

    first_day

--- P_I_F/P_I_F/MainWindow.xaml.cs
+++ P_I_F/P_I_F/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace P_I_F
 {
@@ -20,9 +22,34 @@ namespace P_I_F
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string selectfolder;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public string Read(string path)
+        {
+            return File.ReadAllText(path);
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Open__Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderopen = new FolderBrowserDialog();
+            folderopen.Description = "검색할 폴더";
+            folderopen.ShowDialog();
+
+            selectfolder = folderopen.SelectedPath;
+            string[] filelist = Directory.GetFiles(selectfolder);
+            string h = filelist[0].Substring((filelist[0].Length - 3), 3);
+            list.Items.Add(h);
+            Console.WriteLine();
         }
     }
 }
